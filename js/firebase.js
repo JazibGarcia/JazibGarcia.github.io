65158fb149e2d001bec46012318b1afb2918a62d
JazibGarcia <86211751+JazibGarcia@users.noreply.github.com>
2021-06-20 20:28:41 -0500

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAVlbTHFR6VUJSKM2Wqx21p15btL7KPImk",
+    authDomain: "tu-ropa-online.firebaseapp.com",
+    projectId: "tu-ropa-online",
+    storageBucket: "tu-ropa-online.appspot.com",
+    messagingSenderId: "60281951504",
+    appId: "1:60281951504:web:eef7698396183346e78195",
+    measurementId: "G-XVFR7WQWBP"
+});
